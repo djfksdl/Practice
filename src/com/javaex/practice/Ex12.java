7ad6f3bf0917ff691c3beba5234258b5cb97f64a
djfksdl <djfksdl@gmail.com>
2023-12-26 17:57:34 +0900

    숙제 수정

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -5,7 +5,7 @@ public class Ex12 {
 		/*다음코드의 잘못된 부분을 찾아서 수정해 보세요.
 			(코드를 작성해서 오류가 없도록 변경해 보세요.)
 		 public static void main(String[] agrgs{
-?맞나		 	int x,y = 0; -> int x =0; int y = 0;으로 따로 써주기
+	 		int x,y = 0; -> int x =0; int y = 0;으로 따로 써주기. 이 경우에는 y만 초기화 값을 가짐.
 		 	char grade = "A"; -> char는 작은 따옴표에 써야함 char grade = 'A';
 		 	int salary = 2,000,000; -> ,를 지우고 숫자만 써야한다. int salary =2000000;
 		 	byte n = 1000; -> byte의 범위는 -128~127까지다.
--- src/com/javaex/practice/Ex35.java
+++ src/com/javaex/practice/Ex35.java
@@ -5,7 +5,7 @@ public class Ex35 {
 	public static void main(String[] args) {
 		//?? long형 변수를 사용하여 빛이 1년동안 진행하는 거리를 계산하여 출력하세요.
 		
-		long light = 300000*86400*365L;
+		long light = 300000L*86400*365; // ?? 300000나 86400뒤에 L을 붙이니 답이 나오고 그냥 마지막에 L을 붙이면 안나옴
 		System.out.println("빛이 1년동안 가는 거리는 " + light + " km 입니다.");
 
 	}
